453df82c146ce670399c5c8b0a2ec51d2a1c2ea8
marinefaynot <81978320+marinefaynot@users.noreply.github.com>
2021-04-09 14:02:49 +0200

    Add files via upload

---
+++ accueil.js
@@ -0,0 +1,15 @@
+function ouvrirRegles(){
+	document.getElementById("regles_popup").style.display="block";
+}
+
+function ouvrirInscription(){
+	document.getElementById("inscription_popup").style.display="block";
+}
+
+function ouvrirConnexion(){
+	document.getElementById("connexion_popup").style.display="block";
+}
+
+function revenirPageAccueil(){
+	document.location.href = "accueil.html";
+}
