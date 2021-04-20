e9f4ccce8e0e815ab81354677bf47c604da1fcd3
Momoblb <81977135+Momoblb@users.noreply.github.com>
2021-04-20 15:25:55 +0200

    Add files via upload

---
+++ accueil_jeux/accueil_jeux.js
@@ -0,0 +1,23 @@
+function ouvrirFacile(){
+	document.getElementById("facile_popup").style.display="block";
+}
+
+
+function ouvrirMoyen(){
+	document.getElementById("moyen_popup").style.display="block";
+}
+
+
+function ouvrirDifficile(){
+	document.getElementById("difficile_popup").style.display="block";
+}
+
+
+function ouvrirRegles(){
+	document.getElementById("regles_popup").style.display="block";
+}
+
+
+function revenirPageAccueil(){
+	document.location.href = "accueil_jeux.html";
+}
