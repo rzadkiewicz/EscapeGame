ccaa84de9170f2494617509324536b41aabfd12b
snaMThomas <81978579+snaMThomas@users.noreply.github.com>
2021-05-15 17:12:21 +0200

    Update salle_facile.js

--- Escape_Game/js/salle_facile.js
+++ Escape_Game/js/salle_facile.js
@@ -202,7 +202,7 @@ function SalleFinie() {
 function NvSalle(){
     
     document.getElementById("reponse").value ="";
-
+    closeIndice(indice);
     document.getElementById("livre").style.visibility ="visible";
     document.getElementById("boutonskip").style.visibility ="visible";
     document.getElementById("cadenasouvert").style.visibility ="hidden";
