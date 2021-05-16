259a3d1de477f0f3dba3178f9c2d1a50104d2cc4
Momoblb <81977135+Momoblb@users.noreply.github.com>
2021-05-16 23:13:33 +0200

    Update Database.php

--- Escape_Game/php/app/Database.php
+++ Escape_Game/php/app/Database.php
@@ -1,6 +1,5 @@
 <?php
 
-namespace App;
 
 class Database{
 
@@ -10,6 +9,8 @@ class Database{
 
 	private $db_host;
 
+	private $pdo;
+
 
 	public function __construct($db_name, $db_user = 'root', $db_host = 'localhost') {
 
@@ -18,8 +19,46 @@ class Database{
 		$this->db_user =$db_user;
 
 		$this->db_host =$db_host;
+    }
+
+	private function getPDO(){
+		
+		if($this->pdo === null){
+
+		 $pdo = new PDO('mysql:dbname=escapegame;host=localhost', 'root');
+
+		 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		 $this->pdo = $pdo;
+
+		}
+	
+		return $this->pdo;
+      
+	}
+
+
+	public function query($statement){
+
+		$res = $this->getPDO()->query($statement);
+
+		
 
 
 
 	}
+    
+    public function queryR($statement){
+
+		$rest = $this->getPDO()->query($statement);
+
+		$datas = $rest->fetchALL(PDO::FETCH_OBJ);
+
+		return $datas;
+
+    }
+
+
+
+
 }
