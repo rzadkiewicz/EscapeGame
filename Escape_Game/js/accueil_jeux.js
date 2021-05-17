e017f05f1c7afa9bf5ac35f3c1382723d42285d3
Momoblb <81977135+Momoblb@users.noreply.github.com>
2021-05-17 11:05:59 +0200

    Update accueil_jeux.js

--- Escape_Game/js/accueil_jeux.js
+++ Escape_Game/js/accueil_jeux.js
@@ -1,22 +1,14 @@
-function ouvrirFacile(){
-	document.getElementById("facile_popup").style.display="block";
-}
-
-
-function ouvrirMoyen(){
-	document.getElementById("moyen_popup").style.display="block";
-}
-
-
-function ouvrirDifficile(){
-	document.getElementById("difficile_popup").style.display="block";
-}
-
-
 function ouvrirRegles(){
 	document.getElementById("regles_popup").style.display="block";
 }
 
+function ouvrirParamètre(){
+	document.getElementById("paramètre_popup").style.display="block";
+}
+
+function Déconnexion(){
+	document.getElementById("déconnexion_popup").style.display="block";
+}
 
 function revenirPageAccueil(){
 	document.location.href = "http://localhost/Escape_Game/html/accueil_jeux.php";
